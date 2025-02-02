f5c5e215285ce48729a009fcb4bc3661dc09a6bd
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2025-02-02 06:57:06 +0200

    UI updates

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between md:justify-around bg-[#0E131E] py-[0.5rem] px-[1rem] md:px-0 flex-wrap border-b-1 border-b-[#A2A4A8]'>
-      <Link to="/" className='flex items-center gap-[0.5rem] gradient-text text-2xl font-bold textShadow'>
+      <Link to="/" className='flex items-center gap-[0.5rem] gradient-text text-2xl font-bold textShadow' onClick={() => setMenuState(false)}>
         <i className='fas fa-shield'></i>
         VerifyInfluencers
       </Link>
@@ -17,25 +17,25 @@ const Navbar = () => {
             : 'hidden'
         } md:flex flex-col md:flex-row text-[#A2A4A8] gap-[2rem]`}
       >
-        <Link to='/leaderboard' className='hover:underline'>
+        <Link to='/leaderboard' className='hover:underline' onClick={() => setMenuState(false)}>
           Leaderboard
         </Link>
-        <Link to='/products' className='hover:underline'>
+        <Link to='/products' className='hover:underline' onClick={() => setMenuState(false)}>
           Products
         </Link>
-        <Link to='/monetization' className='hover:underline'>
+        <Link to='/monetization' className='hover:underline' onClick={() => setMenuState(false)}>
           Monetization
         </Link>
-        <Link to='/about' className='hover:underline'>
+        <Link to='/about' className='hover:underline' onClick={() => setMenuState(false)}>
           About
         </Link>
-        <Link to='/contact' className='hover:underline'>
+        <Link to='/contact' className='hover:underline' onClick={() => setMenuState(false)}>
           Contact
         </Link>
-        <Link to='/admin' className='hover:underline'>
+        <Link to='/admin' className='hover:underline' onClick={() => setMenuState(false)}>
           Admin
         </Link>
-        <Link to='/sign-out' className='hover:underline'>
+        <Link to='/sign-out' className='hover:underline' onClick={() => setMenuState(false)}>
           <img />
           <i className='fas fa-arrow-right-from-bracket'></i> Sign Out
         </Link>
